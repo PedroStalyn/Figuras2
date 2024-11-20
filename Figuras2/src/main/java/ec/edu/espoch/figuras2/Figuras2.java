c6d8c954f38233d8d491c7987d7b19dd6c01ab96
Pedro Aguilar <pedro_stalyn@hotmail.com>
2024-11-20 14:10:11 -0500

    Creacion del proyecto

---
+++ Figuras2/src/main/java/ec/edu/espoch/figuras2/Figuras2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.figuras2;
+
+/**
+ *
+ * @author Pedro
+ */
+public class Figuras2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
